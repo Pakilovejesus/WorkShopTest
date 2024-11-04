7238447e90bb68a9f40e635adca163cf73da564e
Pasquale <pakilovejesus@icloud.com>
2024-11-04 10:34:13 +0100

    Initial Commit

---
+++ WorkShopTest/WorkShopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkShopTestApp.swift
+//  WorkShopTest
+//
+//  Created by Pasquale Mirabelli on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkShopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkShopTestTests/WorkShopTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkShopTestTests.swift
+//  WorkShopTestTests
+//
+//  Created by Pasquale Mirabelli on 04/11/24.
+//
+
+import Testing
+@testable import WorkShopTest
+
+struct WorkShopTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
